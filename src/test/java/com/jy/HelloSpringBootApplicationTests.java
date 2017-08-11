c7d11f0044de5712d6c6c9facdf6957cb24224dc
amen <362961910@qq.com>
2017-08-11 19:15:17 +0800

    mock api 请求单元测试

--- src/test/java/com/jy/HelloSpringBootApplicationTests.java
+++ src/test/java/com/jy/HelloSpringBootApplicationTests.java
@@ -2,15 +2,28 @@ package com.jy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class HelloSpringBootApplicationTests {
 
+
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void queryGirlList() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/girl/list"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("abc"));
 	}
 
 }
